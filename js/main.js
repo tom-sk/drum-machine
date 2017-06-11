39b074111a367a20a7e3d208a0c88bad88756c34
Tom <thomasscullyking@hotmail.com>
2017-06-11 19:38:55 +0100

    Samples Added
    Added samples and controlls, plus a clear pattern button

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 var keys = new Tone.MultiPlayer({
     urls : {
-        "hat" : "../samples/hat.wav",
-        "snare" : "../samples/snare.wav",
-        "kick" : "../samples/kick.wav"
+        "ht" : "../samples/artoms21.wav",
+        "mt" : "../samples/artoms18.wav",
+        "lt" : "../samples/artoms15.wav",
+        "hat" : "../samples/vntg_hts03.wav",
+        "hat2" : "../samples/clhat.wav",
+        "openHat" : "../samples/ophat.wav",
+        "clap" : "../samples/clap.wav",
+        "snare" : "../samples/arsn.wav",
+        "kick" : "../samples/kick.wav",
+        "kick2" : "../samples/arkck.wav",
+
     },
     volume : -10,
     fadeOut : 0.1,
@@ -10,8 +18,19 @@ var keys = new Tone.MultiPlayer({
 var synth = new Tone.Synth().toMaster();
 
 var noteNames = Object.keys(keys.buffers._buffers);
-var stepLength = 8;
+// var noteNames = ['hat', 'clhat', 'ophat', 'snare', 'kick', 'hardkick'];
+var stepLength = 16;
 var subDivision = "16n";
+// console.log(Object.keys(keys.buffers._buffers))
+
+
+function arrToObj(array, val){
+  var obj = {};
+  for(var i = 0; i < array.length; i++){
+    obj[array[i]] = val;
+  }
+  return obj;
+}
 
 function stepLengthCalc(num){
   var arr = [];
@@ -20,39 +39,35 @@ function stepLengthCalc(num){
   }
   return arr;
 }
+
+function clearAllNotes() {
+  for (var i = 0; i < 16; i++)
+  {
+    for (var j = 0; j < 10; j++)
+    {
+      matrix1.matrix[i][j] = 0;
+      matrix1.draw()
+    }
+  }
+}
+
 var vel = Math.random() * 0.5 + 0.5;
-
-
 var volume = new Tone.Volume({volume:1});
-var eq = new Tone.EQ3({
-    low:0,
-    mid:0,
-    high:0,
-    lowFrequency:400,
-    highFrequency:2500
-});
+
 Tone.Transport.start();
 Tone.Transport.bpm.value = 120;
 
 keys.chain(volume, Tone.Master);
 
 var vel = 0;
-var newVel = {
-    'kick':1,
-    'snare':1,
-    'hat':1
-};
-var pitch = {
-    'kick':0,
-    'snare':0,
-    'hat':0
-};
+var newVel = arrToObj(noteNames, 1);
+var pitch = arrToObj(noteNames, 0);
+
 var loop = new Tone.Sequence(function(time, col){
 	var column = matrix1.matrix[col];
-    for (var i = 0; i < 4; i++){
+    for (var i = 0; i < 16; i++){
         matrix1.jumpToCol(col);
         if (column[i] === 1){
-            // var vel = Math.random() * 0.5 + 0.5;
             keys.start(noteNames[i], time, 0, "32n", pitch[noteNames[i]], newVel[noteNames[i]]);
         }
     }
@@ -68,7 +83,7 @@ nx.onload = function(){
 
         matrix1.col = stepLengthCalc(stepLength).length;
         matrix1.row = noteNames.length;
-        matrix1.resize($("#content").width(), 250);
+        matrix1.resize($("#content").width(), 450);
         matrix1.init();
         matrix2.init();
         matrix1.draw();
@@ -83,41 +98,101 @@ nx.onload = function(){
         number.set({value: 120});
         number.on('*', function(data){Tone.Transport.bpm.value = data.value;});
 
+        htVel.set({value: 50})
+        htVel.on('*', function(data){
+            newVel['ht'] = data.value / 100;
+        });
+        htPitch.set({value: 5})
+        htPitch.on('*', function(data){
+            pitch['ht'] = convertRange(data.value, [0,10], [-10,10]);
+        });
+        mtVel.set({value: 50})
+        mtVel.on('*', function(data){
+            newVel['mt'] = data.value / 100;
+        });
+        mtPitch.set({value: 5})
+        mtPitch.on('*', function(data){
+            pitch['mt'] = convertRange(data.value, [0,10], [-10,10]);
+        });
+
+        ltVel.set({value: 50})
+        ltVel.on('*', function(data){
+            newVel['lt'] = data.value / 100;
+        });
+        ltPitch.set({value: 5})
+        ltPitch.on('*', function(data){
+            pitch['lt'] = convertRange(data.value, [0,10], [-10,10]);
+        });
+
         hatVel.set({value: 50})
         hatVel.on('*', function(data){
             newVel['hat'] = data.value / 100;
         });
-        snareVel.set({value: 50})
-        snareVel.on('*', function(data){
-            newVel['snare'] = data.value / 100;
-        });
-        kickVel.set({value: 50})
-        kickVel.on('*', function(data){
-            newVel['kick'] = data.value / 100;
-        });
         hatPitch.set({value: 5})
         hatPitch.on('*', function(data){
             pitch['hat'] = convertRange(data.value, [0,10], [-10,10]);
+        });
+        hat2Vel.set({value: 50})
+        hat2Vel.on('*', function(data){
+            newVel['hat2'] = data.value / 100;
+        });
+        hat2Pitch.set({value: 5})
+        hat2Pitch.on('*', function(data){
+            pitch['hat2'] = convertRange(data.value, [0,10], [-10,10]);
+        });
+
+        openHatVel.set({value: 50})
+        openHatVel.on('*', function(data){
+            newVel['openHat'] = data.value / 100;
+        });
+        openHatPitch.set({value: 5})
+        openHatPitch.on('*', function(data){
+            pitch['openHat'] = convertRange(data.value, [0,10], [-10,10]);
+        });
+
+        clapVel.set({value: 50})
+        clapVel.on('*', function(data){
+            newVel['clap'] = data.value / 100;
+        });
+        clapPitch.set({value: 5})
+        clapPitch.on('*', function(data){
+            pitch['clap'] = convertRange(data.value, [0,10], [-10,10]);
+        });
+
+        snareVel.set({value: 50})
+        snareVel.on('*', function(data){
+            newVel['snare'] = data.value / 100;
         });
         snarePitch.set({value: 5})
         snarePitch.on('*', function(data){
             pitch['snare'] = convertRange(data.value, [0,10], [-10,10]);
         });
 
+        kickVel.set({value: 50})
+        kickVel.on('*', function(data){
+            newVel['kick'] = data.value / 100;
+        });
         kickPitch.on('*', function(data){
             pitch['kick'] = data.value;
         });
-        filterLow.set({value: 10})
-        filterLow.on('*', function(data){
-            eq.low.value = convertRange(data.value, [0,10], [-15,0]);
+
+        kick2Vel.set({value: 50})
+        kick2Vel.on('*', function(data){
+            newVel['kick2'] = data.value / 100;
         });
-        filterMid.set({value: 10})
-        filterMid.on('*', function(data){
-            eq.mid.value = convertRange(data.value, [0,10], [-15,0]);
+        kick2Pitch.on('*', function(data){
+            pitch['kick2'] = data.value;
         });
-        filterHigh.set({value: 10})
-        filterHigh.on('*', function(data){
-            eq.high.value = convertRange(data.value, [0,10], [-15,0]);
-        });
+
+        function clearAllNotes() {
+          for (var i = 0; i < 16; i++)
+          {
+            for (var j = 0; j < 7; j++)
+            {
+              matrix1.matrix[i][j] = 0;
+              matrix1.draw()
+            }
+          }
+        }
 
     }
